3d9ae8812a7efa37e852185fc78454247d5c436d
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2022-12-30 11:33:56 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -49,6 +49,6 @@ pushd /home/patch/m8c-rpi4
 popd
 
 # clean up audio routing, wait 5 seconds and shutdown the Raspberry Pi
-killall -s SIGINT jackd alsa_out alsa_in
-sleep 5
-sudo shutdown now
+#killall -s SIGINT jackd alsa_out alsa_in
+#sleep 5
+#sudo shutdown now
