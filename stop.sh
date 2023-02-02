7df48bf81402a8bae59fad78abb17bcf58430e50
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2023-02-02 22:20:59 +0000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Get the PID of the M8C program
-M8C_PID=$(pidof m8c)
-
 # Terminate the M8C program
-kill $M8C_PID
+killall -s SIGINT m8c
 
 # Clean up audio routing
 killall -s SIGINT alsa_out alsa_in
